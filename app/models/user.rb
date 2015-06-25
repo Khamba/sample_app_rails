843857c18af7c94cdf295b5f653af0c9e01eca92
Saurabh Sikchi <saurabhrsikchi@gmail.com>
2015-06-25 23:38:04 +0530

    Add user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ActiveRecord::Base
 
   has_many :microposts
+  has_many :active_relationships, class_name: "Relationship", foreign_key: 'follower_id', dependent: :destroy
+  has_many :passive_relationsips, class_name: "Relationship", foreign_key: 'followed_id', dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationsips, source: :follower
 
   attr_accessor :remember_token
 
@@ -38,7 +42,22 @@ class User < ActiveRecord::Base
   end
 
   def feed
-    Micropost.where("user_id=?", id)
+    following_ids = "SELECT followed_id FROM relationships WHERE follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids}) OR user_id = :user_id", user_id: id)
+  end
+
+  #Follow another user
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user.id)
+  end
+
+  #Unfollow a user
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
   end
 
 end
